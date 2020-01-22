2d1061adba5c1672c5f3532f76917df2f5864514
Ebagcoder <ebagcoder.work@gmail.com>
2020-01-22 12:48:44 -0500

    Create CommonProxy.java

---
+++ src/main/java/com/ebagcoder/More-Loot-Mod/proxy/CommonProxy.java
@@ -0,0 +1,8 @@
+package com.ebagcoder.More-Loot-Mod.proxy;
+
+import net.minecraft.item.Item;
+
+public class CommonProxy 
+{
+	public void registerItemRenderer(Item item, int meta, String id) {}
+}
