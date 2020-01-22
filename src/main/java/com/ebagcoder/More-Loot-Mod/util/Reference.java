d13a4f92bf16ef7956ce1db3be827f85cc7f7afc
Ebagcoder <ebagcoder.work@gmail.com>
2020-01-22 12:53:37 -0500

    Create Reference.java

---
+++ src/main/java/com/ebagcoder/More-Loot-Mod/util/Reference.java
@@ -0,0 +1,12 @@
+package com.ebagcoder.More-Loot-Mod.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "ML";
+	public static final String NAME = "More Loot Mod";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.ebagcoder.More-Loot-Mod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.ebagcoder.More-Loot-Mod.proxy.CommonProxy";
+	
+}
