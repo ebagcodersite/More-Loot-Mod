10bfb8bddad84855a8835dbcc84bab64a19316b9
Ebagcoder <ebagcoder.work@gmail.com>
2020-01-22 12:56:17 -0500

    Create Main.java

---
+++ src/main/java/com/ebagcoder/More-Loot-Mod/Main.java
@@ -0,0 +1,45 @@
+package com.ebagcoder.More-Loot-Mod;
+
+import com.ebagcoder.More-Loot-Mod.init.ModRecipes;
+import com.ebagcoder.More-Loot-Mod.proxy.CommonProxy;
+import com.ebagcoder.More-Loot-Mod.util.Reference;
+import com.ebagcoder.More-Loot-Mod.world.ModWorldGen;
+
+import net.minecraft.init.MobEffects;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+	
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void PreInit(FMLPreInitializationEvent event)
+	{
+		GameRegistry.registerWorldGenerator(new ModWorldGen(), 3);
+	}
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent event)
+	{
+		ModRecipes.init();
+	}
+	
+	@EventHandler
+	public static void Postinit(FMLPostInitializationEvent event)
+	{
+
+	}
+
+}
