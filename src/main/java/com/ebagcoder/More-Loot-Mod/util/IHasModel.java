e0066ba2e32dd05243c5300e23da2a87f6bd8e11
Ebagcoder <ebagcoder.work@gmail.com>
2020-01-22 12:52:28 -0500

    Create IHasModel.java

---
+++ src/main/java/com/ebagcoder/More-Loot-Mod/util/IHasModel.java
@@ -0,0 +1,11 @@
+package com.ebagcoder.More-Loot-Mod.util;
+
+import java.util.Random;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+public interface IHasModel 
+{
+	public void registerModels();
+}
